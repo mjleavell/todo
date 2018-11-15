243a00fa20b9bba32cb8af809e23997e7a413974
Maggie Leavell <mjleavell@gmail.com>
2018-11-15 17:27:33 -0600

    setup complete

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import firebase from 'firebase/app';
+import 'bootstrap';
+
+// import apiKeys from '../db/apiKeys.json';
+
+import './index.scss';
+
+const initApp = () => {
+  console.log('yo');
+};
+
+initApp();
