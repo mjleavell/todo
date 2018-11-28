68c57912f273d09a00e0c26e2e783296a85f91ce
Maggie Leavell <mjleavell@gmail.com>
2018-11-28 13:36:12 -0600

    edit input on card workinggit add .git add .

--- src/components/AddEditTask/addEditTask.js
+++ src/components/AddEditTask/addEditTask.js
@@ -7,14 +7,14 @@ const formBuilder = (task) => {
   const formString = `
   <div class="form-inline mx-auto">
     <label for="form-task-title">Task:</label>
-    <input type="text" class="form-control w-25" value="${task.task}" id="form-task-title" placeholder="Enter new item">
+    <input type="text" class="form-control form-input w-25" value="${task.task}" id="form-task-title" placeholder="Enter new item">
   </div>`;
   return formString;
 };
 
 const getTaskFromForm = () => {
   const task = {
-    task: $('#form-task-title').val(),
+    task: $('.form-input').val(),
     timeStamp: currentTime.getCurrentTime(),
     isCompleted: false,
   };
@@ -48,24 +48,6 @@ const addTask = () => {
     });
 };
 
-// const displayEditForm = (e) => {
-//   const editBtn = $(e.target).closest('.edit-btn');
-//   const idToEdit = $(editBtn).data('edit-id');
-//   taskData.getSingleTask(idToEdit)
-//     .then((singleTask) => {
-//       let domString = '<h3>Edit Task</h3>';
-//       domString += formBuilder(singleTask);
-// eslint-disable-next-line max-len
-//       domString += `<button class="btn btn-primary" id="save-edit-btn" data-single-edit-id=${singleTask.id}>Save Task</button>`;
-//       domString += '<button class="btn btn-secondary" id="cancel-edit-btn">Cancel</button>';
-//       $('#form').html(domString).show();
-//       $('#task').hide();
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
-
 const editTask = (e) => {
   const editedTask = getTaskFromForm();
   const taskId = e.target.dataset.singleEditId;
@@ -80,15 +62,27 @@ const editTask = (e) => {
     });
 };
 
-const editEvent = (e) => {
-  // $('body').on('click', '.edit-btn', (e) => {
+const changeTaskTitleToInput = (e) => {
   const taskCard = $(e.target).closest('.edit-btn').parents().closest('.card');
   const taskTitle = $(taskCard).children().closest('.card-title');
   const taskTitleElement = $(taskTitle).get(0);
   const taskTitleText = $(taskTitleElement).html();
-  $(taskTitleElement).replaceWith(`<input type="text" value="${taskTitleText}" class="form-control edit-input" id="form-task-title">`);
-  // $(editInput).val(taskTitleText);
-  // });
+  const taskId = $(e.target).closest('.edit-btn').data('edit-id');
+  $(taskTitleElement).replaceWith(`<input type="text" value="${taskTitleText}" data-edit-input-id=${taskId} class="form-control form-input edit-input" id="form-task-title">`);
+};
+
+const editInputEnterEvent = (e) => {
+  if (e.keyCode === 13) {
+    const editedTaskId = $(e.target).data('edit-input-id');
+    const editedTaskObject = getTaskFromForm();
+    taskData.updateTask(editedTaskObject, editedTaskId)
+      .then(() => {
+        initializeTasksPage();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 };
 
 const cancelTaskEdit = () => {
@@ -96,8 +90,9 @@ const cancelTaskEdit = () => {
   $('#task').show();
 };
 
+$('body').on('keyup', '.edit-input', editInputEnterEvent);
 $('body').on('click', '#save-new-task', addTask);
-$('body').on('click', '.edit-btn', editEvent);
+$('body').on('click', '.edit-btn', changeTaskTitleToInput);
 $('body').on('click', '#save-edit-btn', editTask);
 $('body').on('click', '#cancel-edit-btn', cancelTaskEdit);
 
