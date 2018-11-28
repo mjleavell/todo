98dfc9e178c8c1cd4ccca4a8dac568e50a895e7b
Maggie Leavell <mjleavell@gmail.com>
2018-11-28 13:01:24 -0600

    task title changing to input field

--- src/components/AddEditTask/addEditTask.js
+++ src/components/AddEditTask/addEditTask.js
@@ -5,7 +5,7 @@ import currentTime from '../../helpers/currentTime';
 
 const formBuilder = (task) => {
   const formString = `
-  <div class="form-group form-inline">
+  <div class="form-inline mx-auto">
     <label for="form-task-title">Task:</label>
     <input type="text" class="form-control w-25" value="${task.task}" id="form-task-title" placeholder="Enter new item">
   </div>`;
@@ -48,21 +48,23 @@ const addTask = () => {
     });
 };
 
-const displayEditForm = (e) => {
-  const editBtn = $(e.target).closest('.edit-btn');
-  const idToEdit = $(editBtn).data('edit-id');
-  taskData.getSingleTask(idToEdit)
-    .then((singleTask) => {
-      let domString = '<h3>Edit Task</h3>';
-      domString += formBuilder(singleTask);
-      domString += `<button class="btn btn-primary" id="save-edit-task" data-single-edit-id=${singleTask.id}>Save Task</button>`;
-      $('#form').html(domString).show();
-      $('#task').hide();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+// const displayEditForm = (e) => {
+//   const editBtn = $(e.target).closest('.edit-btn');
+//   const idToEdit = $(editBtn).data('edit-id');
+//   taskData.getSingleTask(idToEdit)
+//     .then((singleTask) => {
+//       let domString = '<h3>Edit Task</h3>';
+//       domString += formBuilder(singleTask);
+// eslint-disable-next-line max-len
+//       domString += `<button class="btn btn-primary" id="save-edit-btn" data-single-edit-id=${singleTask.id}>Save Task</button>`;
+//       domString += '<button class="btn btn-secondary" id="cancel-edit-btn">Cancel</button>';
+//       $('#form').html(domString).show();
+//       $('#task').hide();
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
 
 const editTask = (e) => {
   const editedTask = getTaskFromForm();
@@ -78,8 +80,25 @@ const editTask = (e) => {
     });
 };
 
+const editEvent = (e) => {
+  // $('body').on('click', '.edit-btn', (e) => {
+  const taskCard = $(e.target).closest('.edit-btn').parents().closest('.card');
+  const taskTitle = $(taskCard).children().closest('.card-title');
+  const taskTitleElement = $(taskTitle).get(0);
+  const taskTitleText = $(taskTitleElement).html();
+  $(taskTitleElement).replaceWith(`<input type="text" value="${taskTitleText}" class="form-control edit-input" id="form-task-title">`);
+  // $(editInput).val(taskTitleText);
+  // });
+};
+
+const cancelTaskEdit = () => {
+  $('#form').hide();
+  $('#task').show();
+};
+
 $('body').on('click', '#save-new-task', addTask);
-$('body').on('click', '.edit-btn', displayEditForm);
-$('body').on('click', '#save-edit-task', editTask);
+$('body').on('click', '.edit-btn', editEvent);
+$('body').on('click', '#save-edit-btn', editTask);
+$('body').on('click', '#cancel-edit-btn', cancelTaskEdit);
 
 export default displayForm;
